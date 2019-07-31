29dbe711acd1c4c3814044dc451020abb36c6b14
Alvaro Garcia Gomez <maxpowel@gmail.com>
2019-08-01 01:31:01 +0200

    Adapted to new applauncher

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
 setup(
-  name='mqtt_bundle',
-  packages=['mqtt_bundle'],
-  version='1.00',
-  description='mqtt support for applauncher',
-  author='Alvaro Garcia Gomez',
-  author_email='maxpowel@gmail.com',
-  url='https://github.com/applauncher-team/mqtt_bundle',
-  download_url='https://github.com/applauncher-team/fluent_bundle',
-  keywords=['mqtt', 'paho'],
-  classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System', 'Topic :: Utilities'],
-  install_requires=['applauncher', 'paho-mqtt']
+    name='mqtt_bundle',
+    packages=['mqtt_bundle'],
+    version='2.0',
+    description='mqtt support for applauncher',
+    author='Alvaro Garcia Gomez',
+    author_email='maxpowel@gmail.com',
+    url='https://github.com/applauncher-team/mqtt_bundle',
+    download_url='https://github.com/applauncher-team/mqtt_bundle/archive/master.zip',
+    keywords=['applauncher', 'bundle', 'mqtt'],
+    classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System',
+                 'Topic :: Utilities'],
+    install_requires=install_requires
 )
